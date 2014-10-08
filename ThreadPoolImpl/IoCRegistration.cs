0294f77bef8a3a4cd8c8ea340bda4476d6627161
unknown <tea@.(none)>
2014-10-08 19:05:36 +0700

    Thread pool adapter has been implemented.

---
+++ ThreadPoolImpl/IoCRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+
+namespace HWdTech.DS.Internals.Implementation
+{
+    class ThreadPoolRegistrationModule: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            IThreadPool pool = new ThreadPoolImpl();
+
+            builder.RegisterInstance(pool).As<IThreadPool>();
+        }
+    }
+}
---
+++ ThreadPoolImpl/ThreadPool.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HWdTech.DS.Internals.Implementation
+{
+    public class ThreadPoolImpl : IThreadPool
+    {
+        public void StartTask(Action<object> action, object args = null)
+        {
+            System.Threading.ThreadPool.QueueUserWorkItem(new System.Threading.WaitCallback(action), args);
+        }
+    }
+
+}
