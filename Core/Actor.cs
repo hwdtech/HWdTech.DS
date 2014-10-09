6ef0bd03d20d1a393635c26bdb35dcde5dee81a9
hwdtech <etyumentcev@hwdtech.ru>
2014-10-09 19:31:25 +0700

    First version of actor's implementation and its performance tests has been added.

---
+++ Core/Actor.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+
+namespace HWdTech.DS.Core
+{
+    public abstract class Actor
+    {
+        int requests = 0;
+        int isHandler = 0;
+        ConcurrentQueue<IMessage> queue = new ConcurrentQueue<IMessage>();
+
+        public void Receive(IMessage message)
+        {
+            bool taskNotQueued = true;
+            Interlocked.Increment(ref requests);
+            if (0 == Interlocked.Exchange(ref isHandler, 1))
+            {
+                Handle(message);
+
+                ProcessQueueTillCanRead();
+
+                Interlocked.Exchange(ref isHandler, 0);
+
+                if (queue.Count > 0)
+                {
+                    taskNotQueued = false;
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(this.ProcessQueue));
+                }
+            }
+            else
+            {
+                queue.Enqueue(message);
+            }
+            if (0 == Interlocked.Decrement(ref requests))
+            {
+                if (taskNotQueued)
+                {
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(this.ProcessQueue));
+                }
+            }
+        }
+
+        private void ProcessQueueTillCanRead()
+        {
+            IMessage m;
+            while (queue.TryDequeue(out m))
+            {
+                Handle(m);
+            }
+        }
+
+        internal void ProcessQueue(object o)
+        {
+            if (0 == Interlocked.Exchange(ref isHandler, 1))
+            {
+                ProcessQueueTillCanRead();
+
+                Interlocked.Exchange(ref isHandler, 0);
+
+                if (queue.Count > 0)
+                {
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(this.ProcessQueue));
+                }
+            }
+        }
+
+
+        protected abstract void Handle(IMessage message);
+    }
+}
---
+++ CoreTests/Actor.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Threading;
+
+using Moq;
+using NUnit.Framework;
+
+namespace HWdTech.DS.Core.Tests
+{
+    [TestFixture]
+    public class ActorTests
+    {
+        MockRepository repository = new MockRepository(MockBehavior.Strict);
+
+        ManualResetEvent waitUntilMessagesAreHandled = new ManualResetEvent(false);
+
+        const int total = 1000000;
+
+        class MockActor : Actor
+        {
+            ManualResetEvent signal;
+
+            public MockActor(ManualResetEvent signal)
+            {
+                this.signal = signal;
+            }
+
+            int counter = 0;
+
+            protected override void Handle(IMessage m)
+            {
+                if (Interlocked.Increment(ref counter) == total)
+                {
+                    signal.Set();
+                }
+            }
+        }
+
+        class StateLessActor
+        {
+            ManualResetEvent signal;
+
+            public StateLessActor(ManualResetEvent signal)
+            {
+                this.signal = signal;
+            }
+
+            int counter = 0;
+
+            public void Receive(IMessage m)
+            {
+                if (Interlocked.Increment(ref counter) == total)
+                {
+                    signal.Set();
+                }
+            }
+        }
+
+        [Test]
+        public void StateLessActorsLoadTest()
+        {
+            ThreadPool.SetMaxThreads(8, 8);
+            ThreadPool.SetMinThreads(8, 8);
+
+            StateLessActor actor = new StateLessActor(waitUntilMessagesAreHandled);
+
+            Mock<IMessage> messageMock = repository.Create<IMessage>();
+            IMessage message = messageMock.Object;
+
+            DateTime start = DateTime.Now;
+
+            for (int i = 0; i < total; ++i)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(
+                    (o) => { actor.Receive(message); }
+                ));
+            }
+
+            Assert.True(waitUntilMessagesAreHandled.WaitOne(4000));
+            DateTime end = DateTime.Now;
+
+            Console.WriteLine("Execution time of test is {0}", end - start);
+        }
+
+        [Test]
+        public void ActorsLoadTest()
+        {
+            ThreadPool.SetMaxThreads(8, 8);
+            ThreadPool.SetMinThreads(8, 8);
+
+            Actor actor = new MockActor(waitUntilMessagesAreHandled);
+
+            Mock<IMessage> messageMock = repository.Create<IMessage>();
+            IMessage message = messageMock.Object;
+
+            DateTime start = DateTime.Now;
+
+            for (int i = 0; i < total; ++i)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(
+                    (o) => { actor.Receive(message); }
+                ));
+            }
+
+            Assert.True(waitUntilMessagesAreHandled.WaitOne(4000));
+            DateTime end = DateTime.Now;
+
+            Console.WriteLine("Execution time of test is {0}", end - start);
+        }
+
+        [Test]
+        public void ThreadPoolLoadTest()
+        {
+            ThreadPool.SetMaxThreads(8, 8);
+            ThreadPool.SetMinThreads(8, 8);
+
+            int counter = 0;
+
+            DateTime start = DateTime.Now;
+
+            for (int i = 0; i < total; ++i)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(
+                    (o) => {
+                        if (Interlocked.Increment(ref counter) == total)
+                        {
+                            waitUntilMessagesAreHandled.Set();
+                        }
+                    }
+                ));
+            }
+
+            Assert.True(waitUntilMessagesAreHandled.WaitOne(4000));
+            DateTime end = DateTime.Now;
+
+            Console.WriteLine("Execution time of test is {0}", end - start);
+        }
+
+        [Test]
+        public void SingleThreadTest()
+        {
+            int counter = 0;
+
+            DateTime start = DateTime.Now;
+
+            for (int i = 0; i < total; ++i)
+            {
+                ++counter;
+                if (counter == total)
+                {
+                    break;
+                }
+            }
+
+            DateTime end = DateTime.Now;
+
+            Console.WriteLine("Execution time of test is {0}", end - start);
+        }
+    }
+}
