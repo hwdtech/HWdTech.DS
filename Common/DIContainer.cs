308e985e51a41622cc0d27af2d8c70dbd9838aec
hwdtech <etyumentcev@hwdtech.ru>
2014-10-11 14:20:06 +0700

    Class for Singleton pattern. DI adapter for autofac.

---
+++ Common/DIContainer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+using Autofac;
+using Autofac.Core;
+
+namespace HWdTech.Common
+{
+    public class DIContainer
+    {
+        IContainer container;
+
+        public DIContainer(IContainer container)
+        {
+            this.container = container;
+        }
+
+        public T Resolve<T>()
+        {
+            return container.Resolve<T>();
+        }
+
+        public static void Init()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            builder.RegisterAssemblyModules(assemblies);
+
+            Singleton<DIContainer>.Instance = new DIContainer(builder.Build());
+        }
+    }
+}
---
+++ Common/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+namespace HWdTech.Common
+{
+    public class Singleton<T>
+    {
+        static public T Instance
+        {
+            get
+            {
+                if (null == instance)
+                {
+                    lock (lockObject)
+                    {
+                        if (null == instance)
+                        {
+                            try
+                            {
+                                typeof (T).InvokeMember("Init", BindingFlags.Public | BindingFlags.Static | BindingFlags.InvokeMethod, null, null, null);
+                            }
+                            catch (Exception)
+                            {
+                                throw new Exception(
+                                    String.Format(
+                                        "Type {0} should have public static method Init to initialize an instance of the singleton object.", 
+                                        typeof(T).ToString()
+                                ));
+                            }
+
+                        }
+                    }
+                }
+                return instance;
+            }
+            set
+            {
+                instance = value;
+            }
+        }
+        static private T instance;
+        private static object lockObject = new object();
+    }
+}
